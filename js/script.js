f3dbce3894c8fc1ef948039688afa884cf3ef6cb
krisassaurus <orelk@sheridancollege.ca>
2022-10-29 20:44:56 -0400

    Add navigation functionality and rotating portfolio items

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const navToggle = document.querySelector('.header__nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
 });
